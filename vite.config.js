390318269cdc2ba093e072e35121b0509da9e7c3
hiz <kuznecovmatvej717@gmail.com>
2026-07-23 15:15:01 +0500

    Релиз

--- vite.config.js
+++ vite.config.js
@@ -1,44 +1,13 @@
-import { defineConfig } from 'vite';
-import { resolve } from 'path';
-import fs from 'fs';
-import inject from 'vite-plugin-html-inject';
-
-// Функция, которая сама соберет все html файлы из папок для сборки
-function getHtmlInputs(dir, inputFiles = {}) {
-  if (!fs.existsSync(dir)) return inputFiles;
-  const files = fs.readdirSync(dir);
-
-  files.forEach(file => {
-    const res = resolve(dir, file);
-    if (fs.statSync(res).isDirectory()) {
-      getHtmlInputs(res, inputFiles);
-    } else if (file.endsWith('.html')) {
-      // Делаем имя ключа понятным для Rollup
-      const key = res.replace(__dirname, '').replace(/[\/\\]/g, '_').replace('.html', '');
-      inputFiles[key] = res;
-    }
-  });
-  return inputFiles;
-}
-
-const pages = getHtmlInputs(resolve(__dirname, 'pages'));
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [
-    inject() // Включаем плагин для вставок <load src="..." />
-  ],
+  base: '/hiz.log/',
   server: {
     host: '127.0.0.1',
-    port: 3000,
-    open: true // Автоматом откроет браузер при старте локалки
+    port: 3000
   },
   build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        projects: resolve(__dirname, 'projects.html'),
-        ...pages // Подкидываем все остальные страницы из папок
-      }
-    }
+    outDir: 'docs',
+    emptyOutDir: true
   }
-});
+})
